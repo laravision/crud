78d98b343f57761d812c9464923d69b5c624ead9
marwenhlaoui <marwenhlaoui@gmail.com>
2017-06-25 20:18:56 +0100

    v1.0

--- readme.md
+++ readme.md
@@ -1,1 +1,71 @@
 # Laravision Crud system
+
+[![Version](https://img.shields.io/packagist/v/laravision/crud.svg)](https://packagist.org/packages/laravision/crud)
+[![License](https://poser.pugx.org/laravision/crud/license.svg)](https://packagist.org/packages/laravision/crud)
+[![Total Downloads](https://img.shields.io/packagist/dt/laravision/crud.svg)](https://packagist.org/packages/laravision/crud)
+
+Quickly build crud system , controller , models and views using Laravel 5. 
+This package expects that you are using Laravel 5.1 or above.
+
+[![Laravision Crud](https://s25.postimg.org/3vy0o6g0v/crud1.png)](https://github.com/laravision/crud/)
+
+## Install
+
+In order to install Laravel 5 Laravision Crud :
+
+1) You will need to import the laravision/crud package via composer:
+
+```shell
+composer require laravision/crud
+```
+2) Add the service provider to your `config/app.php` file within the `providers` key:
+
+```php
+// ...
+'providers' => [
+    /*
+     * Package Service Providers...
+     */
+
+    Laravision\Crud\CrudServiceProvider::class,
+],
+// ...
+```
+
+## Usage
+
+- Create new CRUD : 
+
+```shell
+php artisan make:crud post
+```
+
+- Create new CRUD with model : 
+
+```shell
+php artisan make:crud post --model=Post
+```
+
+- Create new CRUD with out default views path :
+
+```shell
+php artisan make:crud post --view=admin/post
+```
+
+- fixed special controller namespace :
+
+```shell
+php artisan make:crud Admin/Post
+```
+
+
+## License
+
+Laravision Crud is free software distributed under the terms of the MIT license.
+
+## Note 
+
+Please report any issue you find in the issues page.  
+Pull requests are welcome.
+
+
--- src/console/MakeCrudCommand.php
+++ src/console/MakeCrudCommand.php
@@ -31,47 +31,7 @@ class MakeCrudCommand extends Command
     public function __construct()
     {
         parent::__construct();
-    } 
-
-    public function fire()
-    {
-        $arguments = $this->arguments();
-        $options = $this->option();
-        dd($options);
-        $this->info("Generate Crud");
-        
-        /*require_once __DIR__.'/../load/files/env.php';
- 
-        if(!$this->option('existing')){
-            $this->info("Generating the default authentication");
-            Artisan::call('make:auth');
-            File::Delete(base_path('routes/web.php'));
-            File::deleteDirectory(base_path('resources/views')); 
-        } 
-        require_once __DIR__.'/../load/files/route.php'; 
-        File::copy(__DIR__.'/../packages/app.config.php', base_path('config/app.php')); 
-        require_once __DIR__.'/../load/database.php';  
-        require_once __DIR__.'/../load/files/kernel.php';  
-        File::copy(__DIR__.'/../packages/app.config.auth.php', base_path('config/auth.php'));  
-        File::copyDirectory(__DIR__.'/../packages/middleware', app_path('Http/Middleware'));
-        Artisan::call('key:generate'); 
-        
-        $this->info("Migrating the database tables into your application");
-        Artisan::call('migrate');
-        $process = new Process('php artisan db:seed');
-        $process->run(); 
-        $this->info("Publishing the Project files");
-        Artisan::call('vendor:publish');
-        
-        $this->info("Dumping the autoloaded files and reloading all new files");
-        
-        $process = new Process('composer dump-autoload');
-        $process->run(); 
-        Artisan::call('key:generate');
-        $this->info("Successfully installed Admin Zero! Enjoy :)");*/
-        return;
-        
-    }
+    }  
 
 
 
